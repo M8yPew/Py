28417e8f5593a7f79771b7c930056e1fc72a753d
M8yPew <111277034+M8yPew@users.noreply.github.com>
2023-01-18 23:21:39 +0300

    test1

---
+++ ЧтениеФайлов.py
@@ -0,0 +1,21 @@
+import os
+import datetime
+current = os.getcwd()
+folder_name = 'my_folder'
+file_name = 'recipes.txt'
+full_path = os.path.join(current, folder_name, file_name)
+
+with open(full_path, 'r', encoding='utf-8') as file:
+    cook_book = {}
+    for line in file:
+        food_name = line.strip()
+        ingredient_count = int(file.readline())
+        dishes = []
+        for i in range(ingredient_count):
+           line_with_info = file.readline().strip()
+           ingridient_name, quantity, measure = line_with_info.split(' | ')
+           dishes.append({'ingridient_name': ingridient_name,
+                          'quantity': quantity,
+                          'measure': measure})
+        file.readline()
+        cook_book[food_name] = dishes
